9fe239765b7d33260aa8688f46742ebfe0a6f423
Mikael Grankvist <mikael.grankvist@vaadin.com>
2020-06-17 08:35:26 +0300

    Recreate dataView

--- vaadin-select-flow/src/main/java/com/vaadin/flow/component/select/Select.java
+++ vaadin-select-flow/src/main/java/com/vaadin/flow/component/select/Select.java
@@ -104,8 +104,6 @@ public class Select<T> extends GeneratedVaadinSelect<Select<T>, T> implements
 
     private final KeyMapper<T> keyMapper = new KeyMapper<>();
 
-    private SelectDataView<T> dataView;
-
     private int lastNotifiedDataSize = -1;
 
     private volatile int lastFetchedDataSize = -1;
@@ -515,20 +513,14 @@ public class Select<T> extends GeneratedVaadinSelect<Select<T>, T> implements
      */
     @Override
     public SelectDataView<T> getDataView() {
-        if (dataView == null) {
-            dataView = new SelectDataViewImpl(this::getDataProvider, this);
-        }
-        return dataView;
+        return new SelectDataViewImpl(this::getDataProvider, this);
     }
 
     @Override
     public SelectListDataView<T> getListDataView() {
         if (getDataProvider() instanceof ListDataProvider) {
-            if (dataView == null || !(dataView instanceof ListDataView)) {
-                dataView = new SelectListDataView<>(this::getDataProvider,
+            return new SelectListDataView<>(this::getDataProvider,
                         this);
-            }
-            return (SelectListDataView) dataView;
         }
         throw new IllegalStateException(String.format(
                 "Required ListDataProvider, but got '%s'. Use 'getDataView()' "
